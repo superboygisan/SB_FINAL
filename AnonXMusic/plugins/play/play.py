d8013e0452fcc1fb008a17b8285613a44dcc312d
superboygisan <superboygisan@gmail.com>
2026-06-19 09:31:39 +0530

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -15,12 +15,12 @@ from AnonXMusic.utils.decorators.language import languageCB
 from AnonXMusic.utils.decorators.play import PlayWrapper
 from AnonXMusic.utils.formatters import formats
 from AnonXMusic.utils.inline.play import (
-    botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
+from AnonXMusic.utils.inline.extras import botplaylist_markup
 from AnonXMusic.utils.logger import play_logs
 from AnonXMusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
