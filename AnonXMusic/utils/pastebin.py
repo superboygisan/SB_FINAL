cc4f64b3045702ad969ada77fafda1bd86e2dd43
superboygisan <superboygisan@gmail.com>
2026-07-01 02:42:30 +0530

    Update pastebin.py

--- AnonXMusic/utils/pastebin.py
+++ AnonXMusic/utils/pastebin.py
@@ -12,6 +12,33 @@ async def post(url: str, *args, **kwargs):
                 data = await resp.text()
         return data
 
+async def paste(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
+    return link
+
+####2nd paste code 
+
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def ANNIEBIN(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
+
 
 async def AnonyBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
