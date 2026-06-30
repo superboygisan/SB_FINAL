d0010997c1e41a1cad359a40c453d5cf57b14b27
superboygisan <superboygisan@gmail.com>
2026-06-30 17:46:12 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -12,7 +12,7 @@ import aiohttp
 from ShrutiMusic import LOGGER
 
 YOUR_API_URL = None
-FALLBACK_API_URL = "https://shrutibots.site"
+FALLBACK_API_URL = "https://api01.shrutibots.site"
 
 async def load_api_url():
     global YOUR_API_URL
