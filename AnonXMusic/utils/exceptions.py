820d50f60fd86560a6344f6d4d892d29d2bba5e2
superboygisan <superboygisan@gmail.com>
2026-07-01 02:39:31 +0530

    Update exceptions.py

--- AnonXMusic/utils/exceptions.py
+++ AnonXMusic/utils/exceptions.py
@@ -1,3 +1,11 @@
 class AssistantErr(Exception):
     def __init__(self, errr: str):
         super().__init__(errr)
+
+
+def is_ignored_error(err: Union[Exception, BaseException]) -> bool:
+    if isinstance(err, IGNORED_EXCEPTION_CLASSES):
+        return True
+
+    err_str = str(err).lower()
+    return any(keyword.lower() in err_str for keyword in IGNORED_ERROR_KEYWORDS)
